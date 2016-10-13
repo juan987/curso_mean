cf23255317d5a08f87c9ebcce8cc501dcfd5048a
vagrant <vagrant@localhost.localdomain>
2016-10-13 13:25:32 +0200

    primer commit el 13-10-16 a las 13:25

---
+++ Ejemplos_js/ejercicio_1_13_oct_16/app.js
@@ -0,0 +1,28 @@
+console.log("Hola mundo!");
+
+variablesGlobales = "variableGlobal";
+var variablesGlobalesConVar = "Sigue siendo una variable global";
+
+function mi_funcion_con_var(){
+    var otraVariable = "Esto es una variable local";
+    for(var i = 0; i < 10; i++){
+        console.log("valor de i al final:  " +i);
+    }
+    console.log("valor de i al final:  " +i);
+
+}
+
+function mi_funcion_con_let(){
+    let otraVariable = "Esto es una variable local";
+    for(let i = 0; i < 10; i++){
+        console.log("valor de i al final 1:  " +i);
+    }
+    //Buscar una condicion para evaluar si la variable existe
+    //console.log("valor de i al final 2:  " +i);
+
+}
+
+
+mi_funcion_con_var();
+mi_funcion_con_let();
+console.log("Adios mundo cruel!");
