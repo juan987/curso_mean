3053cc1d0aebd1c3d87dc3209ec934b36215a6e4
juan987 desde casa <olonimu@gmail.com>
2016-10-23 22:44:01 +0200

    commit final del 23oct16

---
+++ Ejemplos_js/Calculadora/js/cal_logica_de_negocio.js
@@ -0,0 +1,17 @@
+function MiCalculadora(){
+    this.sumar = function(num1,num2){
+        return num1+num2;
+    }
+
+    this.restar = function(num1,num2){
+        return num1-num2;
+    }
+
+    this.multiplicar = function(num1,num2){
+        return num1*num2;
+    }
+
+    this.dividir = function(num1,num2){
+        return num1/num2;
+    }
+}
--- Ejemplos_js/Calculadora/js/calculadora_js-controlador.js
+++ Ejemplos_js/Calculadora/js/calculadora_js-controlador.js
@@ -1,37 +1,81 @@
 	var pantalla;
-    var numeroEnDisplay = 0;
-    var memoria = 0;
+    var numeroEnDisplay;
+    var memoria;
+    var mi_calculadora;
+    var bool_operacion_clicked;
+    var bool_estado_inicial;
 	function cargar(){
 		pantalla=document.getElementById("pantalla");
+        numeroEnDisplay = "";
+        memoria = 0;
+        mi_calculadora = new MiCalculadora;
+        bool_operacion_clicked = false;
+        bool_estado_inicial = true;
 	}
 
-    function deliverNumber(numero){
+    function dibujarDisplay(numero){
+        if(bool_operacion_clicked){
+            numeroEnDisplay = "";
+            bool_operacion_clicked = false;
+        }
         numeroEnDisplay += numero;
         actualizaDisplay(numeroEnDisplay);
+    }
+
+    function actualizaDisplay(dato){
+                pantalla.value = dato;
     }
 
 
     function sumar(){
         //alert("sumar");
-        actualizaDisplay(numeroEnDisplay+memoria);
-        actualizaMemoria(numeroEnDisplay+memoria);
+        if(bool_estado_inicial){
+            bool_estado_inicial = false;
+        }
+        if(!bool_operacion_clicked){
+            bool_operacion_clicked = true;
+            memoria = mi_calculadora.sumar(parseInt(numeroEnDisplay),memoria);
+            actualizaDisplay(memoria);
+        }
+
+    }
+
+        function restar(){
+        //alert("restar");
+        if(bool_estado_inicial){
+            bool_estado_inicial = false;
+        }
+            if(!bool_operacion_clicked){
+                bool_operacion_clicked = true;
+                //memoria = mi_calculadora.restar(parseInt(numeroEnDisplay),memoria);
+                memoria = mi_calculadora.restar(memoria, parseInt(numeroEnDisplay));
+                actualizaDisplay(memoria);
+            }
+
+    }
+
+        function multiplicar(){
+        //alert("multiplicar");
+        if(bool_estado_inicial){
+            memoria = 1;
+            bool_estado_inicial = false;
+        }
+        if(!bool_operacion_clicked){
+            bool_operacion_clicked = true;
+            //memoria = mi_calculadora.restar(parseInt(numeroEnDisplay),memoria);
+            memoria = mi_calculadora.multiplicar(memoria, parseInt(numeroEnDisplay));
+            actualizaDisplay(memoria);
+        }
+
     }
 
 
 
     function igual(){
-         alert("igual");       
+        window.alert("igual");    
     }
 
-    function actualizaDisplay(datoEnDisplay){
-        pantalla.value = datoEnDisplay;
-    }
-
-    function actualizaMemoria(valor){
-        memoria = valor;
-    }
-
-    function clear(){
+    function mi_clear(){
         alert("metodo clear");       
         //actualizaDisplay(0);
         //actualizaMemoria(0);
