9e94b9f5fbbdfd8c943e2e40739ebd049dde4715
manana <manana>
2016-11-10 13:08:44 +0100

    formulario terminado 10nov16

--- Ejemplos_js/ejemplo8-nodejs/ejemplo-01-nodejs-base.js
+++ Ejemplos_js/ejemplo8-nodejs/ejemplo-01-nodejs-base.js
@@ -4,3 +4,10 @@ for(let i=0; i < process.argv.length; i++){
 
 
 console.log("Hola mundo, 10nov16");
+
+if (process.argv[2] == 1){
+    console.log("El parametro 2 es:  " +process.argv[2]);
+}else{
+    console.log("El parametro 2 es DISTINTO DE 1, y es:  "
+                    +process.argv[2]);
+}
