038c9eae66a14c29319bcea1ec1b974a9df7a723
manana <manana>
2016-11-11 13:37:07 +0100

    ejemplo 6 http, 11nov16

--- Ejemplos_js/ejemplo8-nodejs/ejemplo-05-listar-ficheros-main.js
+++ Ejemplos_js/ejemplo8-nodejs/ejemplo-05-listar-ficheros-main.js
@@ -6,7 +6,7 @@ let extension = process.argv[3]
 //Se da como entrada el directorio que se quiere listar
 let directorio = process.argv[2];
 
-function queHacerCuandoDevuelvaLosFicheros(){
+function queHacerCuandoDevuelvaLosFicheros(error, ficheros){
     if(error){
         console.error("Error al listar", error);
     }else{
