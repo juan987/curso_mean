2df7e0ae349358a0a68e8703839107ca9b7b6a9c
manana <manana>
2016-12-12 14:01:31 +0100

    Version 1 para cliente con autenticacion basic. 12dic16

---
+++ Ejemplos_js/ejemplo-12-passport/ejemplo02_basic_auth_angular/src/app/basic.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs';
+import 'rxrj/add/operator/map'
+import 'rxrj/add/operator/catch'
+
+@Injectable()
+export class BasicService {
+
+  constructor(private http: Http) { }
+    getResultado(): Observable<string>{
+      //En la cabecera es donde ponemos los temas de seguridad
+      //let cabeceraDeSeguridad = new Headers({'Content-Type':'application/json'});
+      let cabeceraDeSeguridad = new Headers();
+      let username = "luis";
+      let password = "luis";
+      let opcionesDeRequest = new RequestOptions({headers:cabeceraDeSeguridad});
+      cabeceraDeSeguridad.append('Authorization', "Basic " + username +":" +password )
+
+      return this.http.get('http://127.0.0.1:8080/home', opcionesDeRequest)//NO usar localhost:8080!!!!
+      
+              .map((response: Response)=>{
+                return response;
+              })
+              .catch((error:any)=>{
+                console.error("Error!!!!", error);
+                return Observable.throw('Error al procesar la peticion')
+              });
+
+    }
+      
+}
